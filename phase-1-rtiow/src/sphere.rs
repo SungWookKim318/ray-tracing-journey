56361a9331d5aeae55fd025fa94c57317ae86e6f
Michael Kim <michael@protopie.io>
2026-08-19 16:34:00 +0900

    feat: finish 6.2 simplifying ray-shpere equation

--- phase-1-rtiow/src/sphere.rs
+++ phase-1-rtiow/src/sphere.rs
@@ -3,13 +3,23 @@ use crate::Ray;
 
 pub fn hit_sphere(center: &Point3, radius: f32, ray: &Ray) -> f32 {
     let origin_center = *center - ray.origin();
-    let a = ray.direction().dot(ray.direction());
-    let b = -2.0 * ray.direction().dot(origin_center);
-    let c = origin_center.dot(origin_center) - (radius * radius);
-    let discriminant = b * b - (4.0 * a * c); // B^2 - 4ac
-    if discriminant < 0. {
-        -1.
+    // let a = ray.direction().dot(ray.direction());
+    // let b = -2.0 * ray.direction().dot(origin_center);
+    // let c = origin_center.dot(origin_center) - (radius * radius);
+    // let discriminant = b * b - (4.0 * a * c); // B^2 - 4ac
+    // if discriminant < 0. {
+    //     -1.
+    // } else {
+    //     (-b - discriminant.sqrt()) / (2.0 * a)
+    // }
+
+    let a = ray.direction().length_squared();
+    let h = ray.direction().dot(origin_center);
+    let c = origin_center.length_squared() - (radius * radius);
+    let discriminant = h * h - a * c;
+    if discriminant < 0.0 {
+        -1.0
     } else {
-        (-b - discriminant.sqrt()) / (2.0 * a)
+        (h - discriminant.sqrt()) / a
     }
 }
