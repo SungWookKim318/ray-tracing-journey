bae36c3f29e1005f1adf09394ee9efae3d30f5a4
Michael Kim <michael@protopie.io>
2026-08-31 16:24:02 +0900

    feature: finished 11.3, add total internal reflection

--- phase-1-rtiow/src/objects/materials/dielectric.rs
+++ phase-1-rtiow/src/objects/materials/dielectric.rs
@@ -3,12 +3,12 @@ use crate::{color::Color, ray::Ray};
 use super::material::Material;
 
 pub struct Dielectric {
-    refraction_ratio: f32, // Air to Matter's eta ratio (reflective ratio)
+    refractive_index: f32, // Air to Matter's eta ratio (reflective ratio)
 }
 
 impl Dielectric {
-    pub fn new(refraction_ratio: f32) -> Self {
-        Self { refraction_ratio }
+    pub fn new(refractive_index: f32) -> Self {
+        Self { refractive_index }
     }
 }
 
@@ -23,15 +23,23 @@ impl Material for Dielectric {
     ) -> bool {
         *attenuation = Color::new(1.0, 1.0, 1.0);
         let faced_reflacted_ratio = if record.is_front_face {
-            1.0 / self.refraction_ratio
+            1.0 / self.refractive_index
         } else {
-            self.refraction_ratio
+            self.refractive_index
         };
 
         let unit_direction = ray_in.direction().normalize();
-        let refracted = unit_direction.refract(record.normal, faced_reflacted_ratio);
 
-        *scattered = Ray::new(record.point, refracted);
+        let cos_theta = unit_direction.dot(-record.normal).min(1.0);
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+
+        let direction = if faced_reflacted_ratio * sin_theta > 1.0 {
+            unit_direction.refelct(record.normal)
+        } else {
+            unit_direction.refract(record.normal, faced_reflacted_ratio)
+        };
+
+        *scattered = Ray::new(record.point, direction);
         true
     }
 }
