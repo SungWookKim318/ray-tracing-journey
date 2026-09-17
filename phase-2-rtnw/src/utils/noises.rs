849304297212b5a0e22412af836422f936b3ff3e
Michael Kim <michael@protopie.io>
2026-09-17 17:49:54 +0900

    feature: finished 5.3 add Hermitian Smoothing

--- phase-2-rtnw/src/utils/noises.rs
+++ phase-2-rtnw/src/utils/noises.rs
@@ -1,3 +1,4 @@
+pub mod hermitian_perlin;
 pub mod noise;
 pub mod perlin;
 pub mod trilinear_perlin;
---
+++ phase-2-rtnw/src/utils/noises/hermitian_perlin.rs
@@ -0,0 +1,93 @@
+use rand::RngExt;
+
+use crate::vec3::Point3;
+
+use super::noise::Noise;
+
+const POINT_COUNT: usize = 256;
+pub struct HermitianPerlin {
+    random_floats: [f32; POINT_COUNT],
+    perm_x: [usize; POINT_COUNT],
+    perm_y: [usize; POINT_COUNT],
+    perm_z: [usize; POINT_COUNT],
+}
+
+impl Noise for HermitianPerlin {
+    fn noise(&self, position: Point3) -> f32 {
+        let u = position.x - position.x.floor();
+        let v = position.y - position.y.floor();
+        let w = position.z - position.z.floor();
+
+        let u = u * u * (3.0 - 2.0 * u);
+        let v = v * v * (3.0 - 2.0 * v);
+        let w = w * w * (3.0 - 2.0 * w);
+
+        let i = position.x.floor() as isize;
+        let j = position.y.floor() as isize;
+        let k = position.z.floor() as isize;
+
+        let mut trilinear_samples = [0.0f32; 8];
+        for (index, sample) in trilinear_samples.iter_mut().enumerate() {
+            let dk = index as isize / 4 as isize;
+            let dj = index as isize / 2 % 2 as isize;
+            let di = index as isize % 2 as isize;
+            let rand_index = self.perm_x[((i + di) & 255) as usize]
+                ^ self.perm_y[((j + dj) & 255) as usize]
+                ^ self.perm_z[((k + dk) & 255) as usize];
+            *sample = self.random_floats[rand_index];
+        }
+
+        let new_value = trilinear_samples
+            .iter()
+            .enumerate()
+            .fold(0.0f32, |acc, (index, value)| {
+                let k = (index / 4) as f32;
+                let j = (index / 2 % 2) as f32;
+                let i = (index % 2) as f32;
+                let sample_interpolated = (i * u + (1.0 - i) * (1.0 - u))
+                    * (j * v + (1.0 - j) * (1.0 - v))
+                    * (k * w + (1.0 - k) * (1.0 - w))
+                    * value;
+                acc + sample_interpolated
+            });
+        new_value
+    }
+}
+
+impl HermitianPerlin {
+    pub const fn point_count() -> usize {
+        POINT_COUNT
+    }
+
+    fn create_perlin_perm(rng: &mut dyn rand::Rng) -> [usize; POINT_COUNT] {
+        let mut new_perm: [usize; POINT_COUNT] = std::array::from_fn(|index| index);
+        Self::permute(rng, &mut new_perm, POINT_COUNT);
+        new_perm
+    }
+
+    fn perlin_generate_perm(rng: &mut dyn rand::Rng, points: &mut [usize]) {
+        for (index, point) in points.iter_mut().enumerate() {
+            *point = index;
+        }
+
+        Self::permute(rng, points, POINT_COUNT);
+    }
+
+    fn permute(rng: &mut dyn rand::Rng, points: &mut [usize], max_value: usize) {
+        for index in (1..max_value).rev() {
+            let target = rng.random_range(0..=index);
+            points.swap(index, target);
+        }
+    }
+}
+
+impl HermitianPerlin {
+    pub fn new(rng: &mut dyn rand::Rng) -> Self {
+        Self {
+            random_floats: std::array::from_fn(|_| rng.random_range(0.0..1.0)),
+            perm_x: HermitianPerlin::create_perlin_perm(rng),
+            perm_y: HermitianPerlin::create_perlin_perm(rng),
+            perm_z: HermitianPerlin::create_perlin_perm(rng),
+        }
+    }
+}
